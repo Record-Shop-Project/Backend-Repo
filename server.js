2a3442b869db189cb6cef963daa42b7a67bd1537
Vasileios Psychas <vaspsych@gmail.com>
2021-05-04 10:43:21 +0200

    Port port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 8080;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 
 const recordsRouter = require('./routes/recordsRouter');
@@ -14,7 +14,7 @@ const path = require('path');
 
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-app.listen(port || 5000, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
