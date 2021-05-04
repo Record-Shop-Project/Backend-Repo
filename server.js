5789781a7d3b314fc7c0898d1d06b44be9d8d13b
Vasileios Psychas <vaspsych@gmail.com>
2021-05-04 10:37:02 +0200

    entry page

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 const port = 8080;
-const cors = require("cors");
+const cors = require('cors');
 
-const recordsRouter = require("./routes/recordsRouter");
-const usersRouter = require("./routes/usersRouter");
-const authRouter = require("./routes/authRouter");
-const logoutRouter = require("./routes/logout");
-const meRouter = require("./routes/meRouter");
-const orderRouter = require("./routes/orderRouter");
-const path = require("path");
+const recordsRouter = require('./routes/recordsRouter');
+const usersRouter = require('./routes/usersRouter');
+const authRouter = require('./routes/authRouter');
+const logoutRouter = require('./routes/logout');
+const meRouter = require('./routes/meRouter');
+const orderRouter = require('./routes/orderRouter');
+const path = require('path');
 
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
@@ -27,27 +27,30 @@ mongoose
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("Connection to cloud database established!"))
-  .catch((err) => console.log("[ERROR] DB connection failed", err));
+  .then(() => console.log('Connection to cloud database established!'))
+  .catch((err) => console.log('[ERROR] DB connection failed', err));
 
 /**EXPRESS MIDDLEWARE */
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_ORIGIN || "http://localhost:3000",
+    origin: process.env.FRONTEND_ORIGIN || 'http://localhost:3000',
     credentials: true, // allow cookies from your origins
   })
 );
 app.use(cookieParser());
-app.use("/images", express.static("images"));
+app.use('/images', express.static('images'));
 
 /**ROUTES */
-app.use("/users", usersRouter);
-app.use("/records", recordsRouter);
-app.use("/login", authRouter);
-app.use("/logout", logoutRouter);
-app.use("/me", meRouter);
-app.use("/orders", orderRouter);
+app.use('/users', usersRouter);
+app.use('/records', recordsRouter);
+app.use('/login', authRouter);
+app.use('/logout', logoutRouter);
+app.use('/me', meRouter);
+app.use('/orders', orderRouter);
+app.use('/', (req, res) => {
+  res.json('Welcome to the Record Store');
+});
 
 // Error Handling
 app.use(function errorHandler(err, req, res, next) {
